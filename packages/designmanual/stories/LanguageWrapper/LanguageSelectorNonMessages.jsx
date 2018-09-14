f8fda15cb0f84d3dcaee8e7cfccabb0f0ace0a8d
Ivar Borthen <ivarborthen@Ivars-MBP-2.lan>
2018-09-14 13:04:54 +0200

    radiobtns for langselector-non-messages

---
+++ packages/designmanual/stories/LanguageWrapper/LanguageSelectorNonMessages.jsx
@@ -0,0 +1,57 @@
+import React, { Fragment, Component } from 'react';
+import PropTypes from 'prop-types';
+import { RadioButtonGroup } from 'ndla-ui';
+
+const LANGUAGES = [
+  {
+    title: 'Norsk bokmål',
+    value: 'nb',
+  },
+  {
+    title: 'Norsk nynorsk',
+    value: 'nn',
+  },
+  {
+    title: 'Engelsk',
+    value: 'en',
+  },
+];
+
+class LanguageSelectorNonMessages extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: LANGUAGES[0].value,
+    };
+  }
+
+  changeLanguage(value) {
+    this.setState({
+      value,
+    });
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <h2 className="u-heading">Velg språk for labels</h2>
+        <div className="c-filter u-margin-top">
+          <RadioButtonGroup
+            label="Velg språk:"
+            options={LANGUAGES}
+            onChange={value => {
+              this.changeLanguage(value);
+            }}
+          />
+        </div>
+        {this.props.children({ lang: this.state.value })}
+      </Fragment>
+    );
+  }
+}
+
+LanguageSelectorNonMessages.propTypes = {
+  children: PropTypes.func.isRequired,
+};
+
+export default LanguageSelectorNonMessages;
